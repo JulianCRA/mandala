fabac1c520ec4f22c7ecf633f947dee2177c8e97
JulianCRA <jj.juliancr@gmail.com>
2018-04-06 22:39:01 +0700

    code refactoring and functionality added

--- script/mandala.js
+++ script/mandala.js
@@ -1,71 +1,159 @@
-let sections = 24;
+let sections = 16;
 let increment = 2 * Math.PI / sections;
 let hOffset = 0;
 let vOffset = 0;
-let reflect = false;
+
+let reflect = true;
+let correction = false;
+let samples = 50;
+let drawGuides = true;
+
+let ctrl = false;
+
+let cnv;
+let guides;
+
+let currentLine;
+let currentPoints = new Array();;
+
+let curves = new Array();
 
 function setup(){
-    let cnv = createCanvas(windowWidth, windowHeight);
-    cnv.parent('cnv');
-    background('black');
-    hOffset = width/2;
-    vOffset = height/2;
-    reflect = true;
+    cnv = createCanvas(windowWidth, windowHeight);
+    cnv.parent("mandala");
+
+    guides = createGraphics(width, height);
+    
+    hOffset = Math.floor(width / 2);
+    vOffset = Math.floor(height / 2);
+
+    drawBackground();
+    noLoop();
 }
 
 function draw(){
-    drawGuides();
+    
 }
 
-function mouseDragged() 
-{ 
-    if(!reflect){
-        push();
-        translate(hOffset, vOffset);
-        
-        let a = atan2(mouseX - hOffset, mouseY - vOffset);
-        stroke('red');
-        strokeWeight(4);
-        for(let i = 0; i < sections; i++){
-            rotate(increment);
-            line(mouseX - hOffset, mouseY - vOffset, pmouseX - hOffset, pmouseY - vOffset);
-        }
-        
-        pop();
-    }else{
-        push();
-        translate(hOffset, vOffset);
-        
-        let a = atan2(mouseX - hOffset, mouseY - vOffset);
-        stroke('red');
-        strokeWeight(4);
-        for(let i = 0; i < sections; i++){
-           
-            if(i%2 == 0){
-                rotate(increment*2);
-                push();
-                
-                scale(-1.0, 1.0);
-                line(mouseX - hOffset, mouseY - vOffset, pmouseX - hOffset, pmouseY - vOffset);
-                pop();
-            }else{
-                //rotate(increment);
-                line(mouseX - hOffset, mouseY - vOffset, pmouseX - hOffset, pmouseY - vOffset);
-                
-            }
-        }
-        pop();
-    }
-}
+function mousePressed(){
+    
+    currentPoints.push({x:mouseX - hOffset, y:mouseY - vOffset});
 
-function drawGuides(){
     push();
     translate(hOffset, vOffset);
-    stroke('white');
-    strokeWeight(1);
-    for(let i = 0; i < sections; i++){
-        line(0, 0, width, 0);
-        rotate(increment);
-    }
+    stroke(200);
+    strokeWeight(2);
+}
+
+function mouseReleased(){
     pop();
+    commitCurve(correction);
+    currentPoints = new Array();
+    curves.push(currentLine);
+    drawLines();
+}
+
+function mouseDragged(){
+    drawNewLine();
+}
+
+function drawNewLine(){
+    let xpos = mouseX - hOffset;
+    let ypos = mouseY - vOffset;
+    for(let i = 0; i < sections; i++){
+        push();
+        rotate(increment*i);
+        if(i%2 != 0){
+            rotate(increment);
+            scale(1.0, -1.0);
+        }
+        
+        line(currentPoints[currentPoints.length-1].x, currentPoints[currentPoints.length-1].y, xpos, ypos);
+        pop();
+    }
+    currentPoints.push({x:xpos, y:ypos});
+}
+
+function commitCurve(){
+    currentLine = createGraphics(width, height);
+    currentLine.translate(hOffset, vOffset);
+    currentLine.stroke(255);
+    currentLine.strokeJoin(ROUND);
+    currentLine.strokeCap(ROUND);
+    currentLine.strokeWeight(3);
+
+    for(let i = 0; i < sections; i++){
+        currentLine.push();
+        currentLine.noFill();
+        currentLine.rotate(increment*i);
+
+        if(i%2 != 0){
+            currentLine.rotate(increment);
+            currentLine.scale(1.0, -1.0);;
+        }
+        
+        if(correction){
+            currentLine.beginShape();
+            currentLine.curveVertex(currentPoints[0].x, currentPoints[0].y);
+            for(let j = 0; j < samples; j++){
+                currentLine.curveVertex(currentPoints[Math.floor(j*currentPoints.length/samples)].x, currentPoints[Math.floor(j*currentPoints.length/samples)].y);
+            }
+            currentLine.curveVertex(mouseX - hOffset, mouseY - vOffset);
+            currentLine.endShape();
+        }
+        else{
+            for(let j = 1; j < currentPoints.length; j++)
+                currentLine.line(currentPoints[j-1].x, currentPoints[j-1].y, currentPoints[j].x, currentPoints[j].y);
+        }
+
+        currentLine.pop();
+    }
+}
+
+function drawLines(){
+    drawBackground();
+    for(let i = 0; i < curves.length; i++){
+        image(curves[i], 0, 0);
+    }
+}
+
+function drawBackground(){
+    guides.background(0);
+    if(drawGuides){
+        guides.stroke(120);
+        guides.strokeWeight(0.3);
+        guides.push();
+        guides.translate(hOffset, vOffset);
+        for(let i = 0; i < sections; i++){
+            guides.line(75, 0, width, 0);
+            guides.rotate(increment);
+        }
+        guides.pop();
+        }
+    image(guides, 0, 0);
+}
+
+function keyPressed(){
+    if (keyCode == CONTROL){ 
+        ctrl = true;
+    }
+    if (keyCode == SHIFT){ 
+        noLoop();
+    }
+}
+
+function keyTyped(){
+    if(ctrl){
+        if (keyCode == 26) {
+            curves.pop();
+            drawLines();
+        }
+    }
+    return false;
+}
+
+function keyReleased(){
+    if(keyCode == CONTROL){
+        ctrl = false;
+    }
 }
